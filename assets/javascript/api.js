f777f244ac3b2b6ba711135908f9c2de8bc5f5e6
Thiago Lemos <lemoos.th@gmail.com>
2022-08-18 22:06:33 +0000

    first commit

---
+++ assets/javascript/api.js
@@ -0,0 +1,13 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = "3db06883836a23e29a70ca9c3c2087ea";
+export const BASE_URL = "https://api.themoviedb.org/3/movie/";
+export const IMG_URL = "https://image.tmdb.org/t/p/w500";
+export const language = "language=pt-BR";
+
+//Trazer o filme em pt-br
+
+// https://api.themoviedb.org/3/movie/200?api_key=3db06883836a23e29a70ca9c3c2087ea&language=pt-BR
+
+// Pegar a imagem do filme
+// https://image.tmdb.org/t/p/w500/jYtNUfMbU6DBbmd4LUS19u4hF4p.jpg
---
+++ assets/javascript/main.js
@@ -0,0 +1,1 @@
+import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
